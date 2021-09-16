cf33775711fdd23d36cd483640242f594ee76992
Bozhou Yu <bozhou.yu@qima.com>
2021-09-16 17:53:02 +0800

    add sort logic

--- src/main/java/com/qima/product/service/ProductService.java
+++ src/main/java/com/qima/product/service/ProductService.java
@@ -3,13 +3,12 @@ package com.qima.product.service;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import com.qima.product.constant.SortDirection;
+import com.qima.product.constant.ProductClassification;
 import com.qima.product.dao.PriorityDao;
 import com.qima.product.dao.ProductDao;
 import com.qima.product.entity.Company;
@@ -67,30 +66,80 @@ public class ProductService {
         // TODO: 3
         return products.stream()
                 .filter(product -> !blacklistedProductIds.contains(product.getId())
-                        && !isCompanyBlacklisted(product.getCompany(), blacklistedCompanyIds))
+                        && !isCompanyContained(product.getCompany(), blacklistedCompanyIds))
                 .collect(Collectors.toList());
     }
 
-    private boolean isCompanyBlacklisted(final Company company, final Set<String> blacklistedCompanyIds) {
+    private boolean isCompanyContained(final Company company, final Set<String> companyIds) {
         if (company == null) {
             return false;
         }
-        return blacklistedCompanyIds.contains(company.getId())
-                || isCompanyBlacklisted(company.getParent(), blacklistedCompanyIds);
+        return companyIds.contains(company.getId()) || isCompanyContained(company.getParent(), companyIds);
     }
 
+    /**
+     * Task 4, mainly for logic checking:
+     * <ol>
+     * <li>Understand priorities are dynamic based on data</li>
+     * <li>Logic about getting comparators based on priority type</li>
+     * <li>Logic to sort products based on comparators one by one if result is 0
+     * (equal)</li>
+     * </ol>
+     */
     private List<Product> sortProducts(final List<Product> products) {
         final List<String> topPrioritizedProdIds = priorityDao.getTopPrioritizedProducts();
         final List<Priority> priorities = priorityDao.getPriorities();
         // TODO: 4
+        final Comparator<Product> comparator = priorities.stream().sorted(Comparator.comparingInt(Priority::getRank))
+                .reduce(getTopComparator(topPrioritizedProdIds),
+                        (c, priority) -> c.thenComparing(getComparator(priority)), (c1, c2) -> c1.thenComparing(c2));
+        products.sort(comparator);
+
         return products;
     }
 
-    private Comparator<Product> getPriceComparator(SortDirection sortDir) {
-        return Comparator.nullsLast(sortDir.convert(Comparator.comparingDouble(Product::getPrice)));
+    private Comparator<Product> getComparator(Priority priority) {
+        switch (priority.getType()) {
+            case PRICE:
+                return getPriceComparator(priority);
+            case CLASSIFICATION:
+                return getClassificationComparator(priority);
+            case COMPANY:
+                return getCompanyComparator(priority);
+            default:
+                return null;
+        }
     }
 
-    private Comparator<Product> getClassificationComparator(SortDirection sortDir) {
-        return Comparator.nullsLast(sortDir.convert(Comparator.comparing(p -> p.getClassification().getRank())));
+    private Comparator<Product> getTopComparator(List<String> topPrioritizedProdIds) {
+        final Map<String, Integer> topProdId2IndexMap = IntStream.range(0, topPrioritizedProdIds.size())
+                .mapToObj(Integer::valueOf).collect(Collectors.toMap(topPrioritizedProdIds::get, Function.identity()));
+        return Comparator
+                .nullsLast(Comparator.comparing(Product::getId, Comparator.comparingInt(topProdId2IndexMap::get)));
+    }
+
+    private Comparator<Product> getPriceComparator(Priority priority) {
+        return Comparator.nullsLast(priority.getDir().convert(Comparator.comparingDouble(Product::getPrice)));
+    }
+
+    private Comparator<Product> getClassificationComparator(Priority priority) {
+        return Comparator.nullsLast(priority.getDir().convert(Comparator.comparing(Product::getClassification,
+                Comparator.comparingInt(ProductClassification::getRank))));
+    }
+
+    private Comparator<Product> getCompanyComparator(Priority priority) {
+        return Comparator.comparing(Product::getCompany, (c1, c2) -> {
+            final boolean isC1Prioritized = isCompanyContained(c1, Set.of(priority.getSource()));
+            final boolean isC2Prioritized = isCompanyContained(c2, Set.of(priority.getSource()));
+            if (isC1Prioritized && !isC2Prioritized) {
+                return 1;
+            }
+
+            if (!isC1Prioritized && isC2Prioritized) {
+                return -1;
+            }
+
+            return 0;
+        });
     }
 }
